10734f06cf8fb90f1a2daaa511396fcf30102d5a
Christian Alic Kelley <cakelley@colorado.edu>
2015-04-06 11:44:18 -0600

    Update project_euler_1.js

--- project_euler_1.js
+++ project_euler_1.js
@@ -1,1 +1,14 @@
-
+var num =[ ];
+for(i = 0; i < 1000; i++)
+  {
+    if((i % 3 === 0) || (i % 5 === 0))
+    {
+        num.push(i);
+    }
+  }
+var sum = 0;
+for(j = 0; j < num.length; j++)
+  {
+        sum += num[j];
+  }
+console.log(sum);
